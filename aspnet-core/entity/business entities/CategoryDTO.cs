5a890920c2e6c90822209895cb8e6c8c24d57ede
Shahed1998 <shahedchowdhury120@gmail.com>
2023-12-23 13:55:10 +0600

    initialized aspnet-core project

---
+++ aspnet-core/entity/business entities/CategoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace entity.business_entities
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Name")]
+        public string? Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Description")]
+        public string? Description { get; set; }
+        [Required]
+        [DisplayName("Image")]
+        public string? Image { get; set; }
+    }
+}
