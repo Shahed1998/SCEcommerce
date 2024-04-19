309e1c4d4eabb0d3be8db76b99939ec6d6eae5a3
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2024-04-20 02:26:39 +0600

    added reactive form to login form

--- source code/angular/src/app/auth/login/login.component.ts
+++ source code/angular/src/app/auth/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+  
+
+  submitForm(){
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+    }
+    else {
+      console.log(this.loginForm)
+    }
+    
+  }
+
+  //#region Getter methods
+  get Username() {
+    return this.loginForm.get('username') as FormControl
+  }
+
+  get Password() {
+    return this.loginForm.get('password') as FormControl
+  }
+  //#endregion
+  
 }
