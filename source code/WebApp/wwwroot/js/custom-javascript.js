ef3fc9232c018b387e86c7a456df1898e2bf2cf1
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2025-04-09 00:55:47 +0600

    added bootbox, global modal, and working on global modal handling

--- source code/WebApp/wwwroot/js/custom-javascript.js
+++ source code/WebApp/wwwroot/js/custom-javascript.js
@@ -1,7 +1,18 @@
-﻿function CreateModal(e, el) {
+﻿function CreateModal(e, el, title="") {
     e.preventDefault();
-    const href = $(el).attr('href');
-    //console.log(href);
-    // add ajax call, so that the modal is loaded dynamically and can be called from anywhere
+
+    $.ajax({
+        url: $(el).attr('href'),
+        beforeSend: function () {
+            // show spiner
+        }
+    }).done(function (result) {
+        $('#globalModalLabel').text(title);
+        $('#globalModal .modal-body').html(result);
+        $('#globalModal').modal('show');
+    }).fail(function () {
+        $('#globalModal').modal('hide');
+        bootbox.alert("An error occured");
+    });
 
 }
