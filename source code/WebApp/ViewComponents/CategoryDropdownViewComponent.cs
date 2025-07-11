a1780c31091b7460f8989ed8a41bcacc7bdf122b
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2025-07-11 21:33:36 +0600

    added category load functionality in explore using ViewComponent

---
+++ source code/WebApp/ViewComponents/CategoryDropdownViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Manager.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    public class CategoryDropdownViewComponent : ViewComponent
+    {
+        private readonly ICategoryManager _categoryManager;
+
+        public CategoryDropdownViewComponent(ICategoryManager categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _categoryManager.All();
+            return View("Default", categories);
+        }
+    }
+}
